c36d72794d6b2c183d4969647ef87215efb2f4e1
syedanaqvi72 <mahekzehra5@gmail.com>
2023-05-04 10:41:15 -0700

    1

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React with mehak 
-        </a>
-      </header>
+    <div className="News">
+      
     </div>
-  );
+  
+ );
 }
 
 export default App;
