ebfa68894e7b51dff0eee809df0320ab4de1ed67
syedanaqvi72 <mahekzehra5@gmail.com>
2023-05-05 05:12:15 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 
 import './App.css';
-
+let name ="Everyone";
 function App() {
   return (
-    <div className="News">Funny news</div>
+    <> 
+    <nav>
+      <li> Home </li>   
+      <li> Contact us</li>
+      <li>About</li>
+          </nav>
+          <div classNAme="container">
+            <h1>Hello {name}
+              </h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, harum ipsam! Optio quos debitis quidem rerum qui voluptas perspiciatis distinctio unde sit culpa tenetur, expedita earum inventore asperiores! Explicabo facere quas expedita ex? Iusto. </p>
+          </div>
+        
+    </>
 
   
  );
