9096f9b2beca13a6384463a209a63d19b5f3735f
syedanaqvi72 <mahekzehra5@gmail.com>
2023-05-07 11:16:35 -0700

    doing 12

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,10 +4,10 @@ export default function TextForm(props) {
   setText("kjhjhhggig"); 
   return (
     <div>
-  <h1>{props.heading} - {text} </h1> 
+  <h1>{props.heading}  </h1> 
 <div className ="mb-3">
   <label htmlfor="Write Here" className="form-label">Enter Your text Here </label>
-  <textarea className="form-control" id="My Box" rows="11"></textarea>
+  <textarea className="form-control" value={ - text} id="My Box" rows="11"></textarea>
 </div>
     <button className = "btn btn-primary"> Convert to upercase </button>
     </div>
